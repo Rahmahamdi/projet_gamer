d9a09433b4008b8785aa6711cf64d13d3303aea4
Rahmahamdi <47861630+Rahmahamdi@users.noreply.github.com>
2022-11-14 01:19:50 +0100

    commit

---
+++ gamer.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once './pages/class.inc.php';
+
+?>
+
+<?php
+
+include_once './pages/head.inc.php'; 
+?>
+<body>
+    <?php include_once './pages/header.inc.php'; ?>
+    <?php include_once './pages/nav.inc.php'; ?>
+    <main>
+    <?php include_once './pages/cont.inc.php'; ?>
+    </main>
+    <?php include './pages/footer.inc.php'; ?>
+</body>
+</html>
